305fe06387617414f60fff23ccfe10fd449ab528
Matthew Welch <dmw9400@gmail.com>
2017-11-22 19:08:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -68,7 +68,8 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if 
+  if selective_tweet_shortener(tweet).length > 140
+    selective_tweet_shortener(tweet)
 
 
 end
